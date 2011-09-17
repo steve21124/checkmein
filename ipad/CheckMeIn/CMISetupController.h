c4e07bb090f731ebd76d48013bbe48e8e8ba1bd2
Antonio MENDES PINTO <mendes_a@Kadoc.local>
2011-09-17 14:20:51 +0200

    Added - ASIHTTPRequest - First files for "Setup" view

---
+++ ipad/CheckMeIn/CMISetupController.h
@@ -0,0 +1,13 @@
+//
+//  CMISetupController.h
+//  CheckMeIn
+//
+//  Created by Antonio MENDES PINTO on 17/09/11.
+//  Copyright (c) 2011 Moodstocks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CMISetupController : UIViewController
+
+@end
