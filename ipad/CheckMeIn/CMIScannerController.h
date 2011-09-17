ea6feb25d5cd4a9478d931aa5910145b062f0a7c
Cédric Deltheil <cedric@moodstocks.com>
2011-09-17 15:37:57 +0200

    Created a scanner controller (scaffold)

---
+++ ipad/CheckMeIn/CMIScannerController.h
@@ -0,0 +1,16 @@
+//
+//  CMIScannerController.h
+//  CheckMeIn
+//
+//  Created by Cedric Deltheil on 17/09/11.
+//  Copyright 2011 Moodstocks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface CMIScannerController : UIViewController {
+    
+}
+
+@end
